1bb73524be30ee2d3d8365b6e4824a1d7cbf5d11
v15hv4 <vishva2912@gmail.com>
2021-09-23 22:10:30 +0530

    Improve error handling for sig

--- src/commands/sig.c
+++ src/commands/sig.c
@@ -1,12 +1,13 @@
 #include <signal.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "commands.h"
 #include "errors.h"
 #include "proc.h"
 
 int sig(int argc, char** argv) {
-    if (argc < 3) throw_custom_error("sig: not enough arguments", -1);
+    if (argc < 3) return throw_custom_error("sig: not enough arguments", -1);
 
     // get job id and signal number as input
     int job_id = atoi(argv[1]);
@@ -14,12 +15,14 @@ int sig(int argc, char** argv) {
 
     // get job using id from job pool
     struct ProcessPool job = get_job(job_id);
-    if (job.id == -1) throw_custom_error("sig: invalid job id", -2);
-    printf("target pid: %d\n", job.process.pid);
+    if (job.id == -1) return throw_custom_error("sig: invalid job id", -2);
 
     // send signal to target process
     if (kill(job.process.pid, signum)) {
-        throw_custom_error("sig", -3);
+        char* error_format = "sig: unable to send signal %d to process with pid %d";
+        char* error_msg = calloc(2 * strlen(error_format), sizeof(char));
+        sprintf(error_msg, error_format, signum, job.process.pid);
+        return throw_custom_error(error_msg, -3);
     }
 
     return 0;
